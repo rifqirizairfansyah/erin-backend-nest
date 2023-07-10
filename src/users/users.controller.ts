393e5284b85c62ff5f5f931e7610de8f09b477c6
rifqirizairfansyah <rizairfansyahrifqi@gmail.com>
2023-07-10 18:17:49 +0700

    update code 200

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -14,6 +14,7 @@ export class UsersController {
     ) {}
 
   @Post()
+  @HttpCode(200)
   async create(@Body() createUserDto: CreateUserDto) {
     const user = await this.usersService.create(createUserDto);
     await this.schedulerService.schedule(
